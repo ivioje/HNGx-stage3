0e90869ec3f80536e2af513e98ebc3ca36cb8f7d
ivioje <rhodaivioje@gmail.com>
2023-09-21 10:38:01 +0100

    fixed loading state for login

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
 import { FaTags } from "react-icons/fa";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
 import Skeleton from "./Skeleton";
+import { AppContext } from "../context/AppContext";
 
 const ImageCard = ({ image, isDragging }) => {
+	const { loading } = useContext(AppContext);
 	return (
 		<div
 			className={`${
@@ -15,13 +17,18 @@ const ImageCard = ({ image, isDragging }) => {
 				<LazyLoadImage
 					src={image.url ? image.url : <Skeleton />}
 					alt={image.tag}
-					className="w-full h-full rounded-md shadow-md"
+					className={`w-full h-full ${
+						loading ? "bg-gray-400" : ""
+					}  rounded-md shadow-md`}
 					effect="blur"
 				/>
 			</div>
 			<div
 				className="grid bg-no-repeat bg-cover bg-center rounded-md shadow-md xs:hidden w-[300px] h-[200px] cursor-grab"
-				style={{ backgroundImage: `url(${image?.url})` }}
+				style={{
+					backgroundImage: `url(${image?.url})`,
+					backgroundColor: "#bbb",
+				}}
 			/>
 
 			<div className="absolute flex items-center p-1 font-medium text-gray-800 lowercase rounded-lg left-4 top-4 bg-white/30 backdrop-blur-md">
