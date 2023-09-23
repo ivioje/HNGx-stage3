83c71c3a33506be0e4bef77426d369edb1b17399
ivioje <rhodaivioje@gmail.com>
2023-09-23 10:18:06 +0100

    feat: added redirect

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 
 import Gallery from "./pages/Gallery";
@@ -33,7 +33,7 @@ const App = () => {
 				/>
 				<Route
 					path="*"
-					element={<Gallery />}
+					element={<Navigate to="/" />}
 				/>
 			</Routes>
 
